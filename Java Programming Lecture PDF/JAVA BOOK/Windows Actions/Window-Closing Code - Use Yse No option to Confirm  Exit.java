22d7f3bc987fc0b7dbe3e5f26e1dab9acd99206e
Nur Hossain Rabbi <nrabbi203016@bscse.uiu.ac.bd>
2026-02-24 03:46:37 +0600

    Add files via upload

---
+++ Java Programming Lecture PDF/JAVA BOOK/Windows Actions/Window-Closing Code - Use Yse No option to Confirm  Exit.java
@@ -0,0 +1,16 @@
+JFrame1.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                int choose = JOptionPane.showConfirmDialog(null,
+                        "Do you really want to exit the application ?",
+                        "Confirm Close", JOptionPane.YES_NO_OPTION,
+                        JOptionPane.INFORMATION_MESSAGE);
+                if (choose == JOptionPane.YES_OPTION) {
+                    e.getWindow().dispose();
+                    System.out.println("close");
+                  
+                } else {
+                    System.out.println("do nothing");
+                }
+            }
+});
